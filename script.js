5400a7d0236a4820a4be9d0538715af6deeac792
Thiago Matos <123104655+DevloperThiago@users.noreply.github.com>
2023-11-28 14:04:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function numberReveal() {
+    const inputValue = document.querySelector('.main-input').value
+    const revealedNumber = document.querySelector('.main-response')
+
+    revealedNumber.innerHTML = inputValue
+}
